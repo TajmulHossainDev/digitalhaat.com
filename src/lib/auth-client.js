566996685b60ddc2ccf2290f0190388a3db85452
TajmulHossainDev <tajmulhossain6991@gmail.com>
2026-05-04 20:40:19 +0300

    trying to fixed deploy

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,7 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    // baseURL সরিয়ে দিন অথবা নিচের মতো করে লিখুন
-    baseURL: process.env.NEXT_PUBLIC_VERCEL_URL 
-             ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` 
-             : "http://localhost:3000"
-})
+  baseURL: process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : "http://localhost:3000",
+});
